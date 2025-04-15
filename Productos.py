78a776dca968a6c45666429bbf9b30ceefceed48
Marianie Vásquez <99088963+Marianie13@users.noreply.github.com>
2025-04-15 16:53:36 -0500

    Se agrega la lista de produtos del fruver

---
+++ Productos.py
@@ -0,0 +1,12 @@
+#lista de produtos (nombre, precio y stock)
+
+productos = [
+    {"nombre": "Fresas", "precio":5000, "stock": 10},
+    {"nombre": "Banano", "precio":2000, "stock": 20},
+    {"nombre": "Uvas",   "precio":8000, "stock": 10},
+    {"nombre": "Coco",   "precio":10000,"stock":30},
+    {"nombre": "Melón",  "precio":6500, "stock": 8},
+    {"nombre": "Sandía", "precio":9000, "stock": 5},
+    {"nombre": "Pera",   "precio":2500, "stock": 34},
+    {"nombre": "Manzana","precio":3200, "stock": 28},
+]
